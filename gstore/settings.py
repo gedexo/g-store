bd3d36735b06d0d6349b632b6e193657be605004
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-15 12:31:31 +0530

    Adding Required apps for different services

--- gstore/settings.py
+++ gstore/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apis',
+    'fashion',
+    'fruits',
+    'hotel',
+    'mobiles',
+    'plumbingandelectrical',
+    'stationary',
+    'vegitable',
 ]
 
 MIDDLEWARE = [
