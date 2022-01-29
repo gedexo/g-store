1885cd5296c01bb616a098982f163dbc4837ea3a
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-29 12:33:30 +0530

    changing Media root

--- gstore/settings.py
+++ gstore/settings.py
@@ -164,8 +164,12 @@ SIMPLE_JWT = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+STATIC_FILE_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ((BASE_DIR / 'static'),)
+STATIC_ROOT = BASE_DIR / 'assets'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
